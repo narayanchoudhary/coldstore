e60fed24d13dca61493b6ffeba2fad6663451120
narayan.choudhary0506@gmail.com <narayan.choudhary0506@gmail.com>
2018-09-16 20:56:35 +0530

    Added glyphicons in the remaining input fileds

--- src/components/UI/glyphicon.js
+++ src/components/UI/glyphicon.js
@@ -1,54 +1,60 @@
 import React, { Component } from 'react'
 
 class Glyphicon extends Component {
-    render () {
+    render() {
         let classes = ['glyphicon'];
-        if(this.props.type === 'name') {
+        if (this.props.type === 'name') {
             classes.push('glyphicon-user')
-        }else if(this.props.type === 'phone') {
-            classes.push('glyphicon-earphone')                     
-        }else if(this.props.type === 'address') {
-            classes.push('glyphicon-home')                      
-        }else if(this.props.type === 'receiptNumber') {
+        } else if (this.props.type === 'phone') {
+            classes.push('glyphicon-earphone')
+        } else if (this.props.type === 'address' || this.props.type === 'addressName') {
+            classes.push('glyphicon-home')
+        } else if (this.props.type === 'receiptNumber') {
             classes.push('glyphicon-tag')
-        }else if(this.props.type === 'date') {
+        } else if (this.props.type === 'date') {
             classes.push('glyphicon-time')
-        }else if(this.props.type === 'party') {
+        } else if (this.props.type === 'party') {
             classes.push('glyphicon-user')
-        }else if(this.props.type === 'item' || this.props.type === 'itemName') {
+        } else if (this.props.type === 'item' || this.props.type === 'itemName') {
             classes.push('glyphicon-grain')
-        }else if(this.props.type === 'variety' || this.props.type === 'varietyName') {
-            classes.push('glyphicon-filter' )
-        }else if(this.props.type === 'size' || this.props.type === 'sizeName') {
+        } else if (this.props.type === 'variety' || this.props.type === 'varietyName') {
+            classes.push('glyphicon-filter')
+        } else if (this.props.type === 'size' || this.props.type === 'sizeName') {
             classes.push('glyphicon-signal')
-        }else if(this.props.type === 'privateMarka') {
+        } else if (this.props.type === 'privateMarka') {
             classes.push('glyphicon-tag')
-        }else if(this.props.type === 'packet') {
+        } else if (this.props.type === 'packet') {
             classes.push('glyphicon-oil')
-        }else if(this.props.type === 'weight') {
+        } else if (this.props.type === 'weight') {
             classes.push('glyphicon-dashboard')
-        }else if(this.props.type === 'motorNumber') {
+        } else if (this.props.type === 'motorNumber') {
             classes.push('glyphicon-bed')
-        }else if(this.props.type === 'remark') {
+        } else if (this.props.type === 'remark') {
             classes.push('glyphicon-edit')
-        }else if(this.props.type === 'chamber') {
+        } else if (this.props.type === 'chamber') {
             classes.push('glyphicon-gift')
-        }else if(this.props.type === 'floor') {
+        } else if (this.props.type === 'floor') {
             classes.push('glyphicon-align-justify')
-        }else if(this.props.type === 'rack') {
+        } else if (this.props.type === 'rack') {
             classes.push('glyphicon-equalizer')
-        }else if(this.props.type === 'merchant') {
+        } else if (this.props.type === 'merchant') {
             classes.push('glyphicon-user')
-        }else if(this.props.type === 'amount') {
+        } else if (this.props.type === 'amount') {
             classes.push('glyphicon-usd')
-        }else if(this.props.type === 'bank') {
+        } else if (this.props.type === 'bank') {
             classes.push('glyphicon-piggy-bank')
-        }else if(this.props.type === 'checkNumber') {
+        } else if (this.props.type === 'checkNumber') {
             classes.push('glyphicon-check')
-        }else if(this.props.type === 'openingBalance') {
+        } else if (this.props.type === 'openingBalance') {
             classes.push('glyphicon-usd')
-        }else if(this.props.type === 'year') {
+        } else if (this.props.type === 'year') {
             classes.push('glyphicon-calendar')
+        } else if (this.props.type === 'bankName') {
+            classes.push('glyphicon-home')
+        } else if (this.props.type === 'ifsc') {
+            classes.push('glyphicon-qrcode')
+        } else if (this.props.type === 'expenseCategory' || this.props.type === 'expenseCategoryName') {
+            classes.push('glyphicon-tags')
         }
         return (
             <i className={classes.join(" ")}></i>
