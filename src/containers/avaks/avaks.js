b197b9366ba7ee48888b1a64f21b9ffd51006f31
narayan.choudhary0506@gmail.com <narayan.choudhary0506@gmail.com>
2018-10-30 11:07:37 +0530

    fixed isue of sort in the receipt number

--- src/containers/avaks/avaks.js
+++ src/containers/avaks/avaks.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 import cellEditFactory, { Type } from 'react-bootstrap-table2-editor';
-import filterFactory, { textFilter, numberFilter,Comparator } from 'react-bootstrap-table2-filter';
+import filterFactory, { textFilter, numberFilter, Comparator } from 'react-bootstrap-table2-filter';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions';
 import './avaks.css';
@@ -25,6 +25,9 @@ class Avaks extends Component {
         mode: 'click',
         blurToSave: true,
         afterSaveCell: (oldValue, newValue, row, column) => {
+
+            row.receiptNumber = parseInt(row.receiptNumber, 10);// so that we don't face any problem in the sorting
+
             if (column.dataField === 'address') {
                 this.props.filterPartiesByAddress(this.props.parties, { value: newValue });
             }
